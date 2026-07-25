4b42a57092e113370fe7c754da1cb4c0413a8cb6
Steve Kinney <hello@stevekinney.net>
2026-07-25 10:10:06 -0600

    fix(mega-menu): preserve keyboard document order

--- packages/playground/src/component-page.svelte
+++ packages/playground/src/component-page.svelte
@@ -1230,10 +1230,10 @@
                     <div class="dx-keys">
                       {#each a11y.keyboard as shortcut, index (index)}
                         <div class="dx-keys__row">
-                          <div class="dx-keys__key-list" role="group" aria-label={shortcut.keys}>
+                          <div class="dx-keys__key-list">
                             {#each shortcut.keys.split(/\s+\/\s+/) as key, keyIndex (keyIndex)}
                               {#if keyIndex > 0}
-                                <span class="dx-keys__separator" aria-hidden="true">/</span>
+                                <span class="dx-keys__separator">/</span>
                               {/if}
                               <Kbd label={key} />
                             {/each}
